f1bb5d00b604a49768f84cac3942d26582f1d9f4
lawliet zou <lawliet.zou@gmail.com>
2014-07-15 09:40:31 +0800

    version 0.2
    update format

--- VC0706.cpp
+++ VC0706.cpp
@@ -1,321 +1,371 @@
+/*
+* VC0706.cpp
+* A library for camera shield(VC0706 chipset)
+*
+* Copyright (c) 2014 seeed technology inc.
+* Copyright (c) 2012, Adafruit Industries.
+*
+* All rights reserved.
+*
+* This library is based on Adafruit's VC0706 Serial Camera Library. Thanks to 
+* their contribution to the code, we modify it to support our Seeed's Camera 
+* Shield module.
+*
+* Below is the introduction of Adafruit's VC0706 Serial Camera module, we add it to here
+* to express our thanks to them.
+*
+* ****************************************************************************
+* This is a library for the Adafruit TTL JPEG Camera (VC0706 chipset)
+*
+* Pick one up today in the adafruit shop!
+* ------> http://www.adafruit.com/products/397
+*
+* These displays use Serial to communicate, 2 pins are required to interface
+*
+* Adafruit invests time and resources providing this open source code, 
+* please support Adafruit and open-source hardware by purchasing 
+* products from Adafruit!
+*
+* Written by Limor Fried/Ladyada for Adafruit Industries.  
+* BSD license, all text above must be included in any redistribution
+* ******************************************************************************
+*
+* Software License Agreement (BSD License)
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holders nor the
+* names of its contributors may be used to endorse or promote products
+* derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
 #include "VC0706.h"
 
 VC0706::VC0706(SoftwareSerial *ser) 
 {
-	camera = ser;
-	frameptr = 0;
-	bufferLen = 0;
+    camera = ser;
+    frameptr = 0;
+    bufferLen = 0;
 }
 
 boolean VC0706::begin(uint16_t baud) 
 {
-	camera->begin(baud);
-	delay(50);
-	camera->write((byte)0x56);
-	delay(500);
-	return reset();
+    camera->begin(baud);
+    delay(50);
+    camera->write((byte)0x56);
+    delay(500);
+    return reset();
 }
 
 boolean VC0706::reset() 
 {
-	uint8_t args[] = {0x0};
-	return runCommand(VC0706_SYSTEM_RESET, args, 1, 5);
+    uint8_t args[] = {0x0};
+    return runCommand(VC0706_SYSTEM_RESET, args, 1, 5);
 }
 
 boolean VC0706::motionDetected() 
 {
-	if (readResponse(4, 200) != 4) {
-		return false;
-	}
-	if (! verifyResponse(VC0706_COMM_MOTION_DETECTED))
-		return false;
+    if (readResponse(4, 200) != 4) {
+        return false;
+    }
+    if (! verifyResponse(VC0706_COMM_MOTION_DETECTED))
+        return false;
 
-	return true;
+    return true;
 }
-
 
 uint8_t VC0706::setMotionStatus(uint8_t x, uint8_t d1, uint8_t d2) 
 {
-	uint8_t args[] = {0x03, x, d1, d2};
-	return runCommand(VC0706_MOTION_CTRL, args, sizeof(args), 5);
+    uint8_t args[] = {0x03, x, d1, d2};
+    return runCommand(VC0706_MOTION_CTRL, args, sizeof(args), 5);
 }
-
 
 uint8_t VC0706::getMotionStatus(uint8_t x) 
 {
-	uint8_t args[] = {0x01, x};
-	return runCommand(VC0706_MOTION_STATUS, args, sizeof(args), 5);
+    uint8_t args[] = {0x01, x};
+    return runCommand(VC0706_MOTION_STATUS, args, sizeof(args), 5);
 }
 
 boolean VC0706::setMotionDetect(boolean flag) 
 {
-	if (! setMotionStatus(VC0706_MOTIONCONTROL, VC0706_UARTMOTION, VC0706_ACTIVATEMOTION))
-		return false;
+    if (! setMotionStatus(VC0706_MOTIONCONTROL, VC0706_UARTMOTION, VC0706_ACTIVATEMOTION))
+        return false;
 
-	uint8_t args[] = {0x01, flag};
-	return runCommand(VC0706_COMM_MOTION_CTRL, args, sizeof(args), 5);
+    uint8_t args[] = {0x01, flag};
+    return runCommand(VC0706_COMM_MOTION_CTRL, args, sizeof(args), 5);
 }
-
-
 
 boolean VC0706::getMotionDetect(void) 
 {
-	uint8_t args[] = {0x0};
-	if (! runCommand(VC0706_COMM_MOTION_STATUS, args, 1, 6))
-		return false;
-	
-	return camerabuff[5];
+    uint8_t args[] = {0x0};
+    if (! runCommand(VC0706_COMM_MOTION_STATUS, args, 1, 6))
+        return false;
+    
+    return camerabuff[5];
 }
 
 uint8_t VC0706::getImageSize() 
 {
-	uint8_t args[] = {0x4, 0x4, 0x1, 0x00, 0x19};
-	if (! runCommand(VC0706_READ_DATA, args, sizeof(args), 6))
-		return -1;
+    uint8_t args[] = {0x4, 0x4, 0x1, 0x00, 0x19};
+    if (! runCommand(VC0706_READ_DATA, args, sizeof(args), 6))
+        return -1;
 
-	return camerabuff[5];
+    return camerabuff[5];
 }
 
 boolean VC0706::setImageSize(uint8_t x) 
 {
-	uint8_t args[] = {0x05, 0x04, 0x01, 0x00, 0x19, x};
-	return runCommand(VC0706_WRITE_DATA, args, sizeof(args), 5);
+    uint8_t args[] = {0x05, 0x04, 0x01, 0x00, 0x19, x};
+    return runCommand(VC0706_WRITE_DATA, args, sizeof(args), 5);
 }
 
 uint8_t VC0706::getDownsize(void) 
 {
-	uint8_t args[] = {0x0};
-	if (! runCommand(VC0706_DOWNSIZE_STATUS, args, 1, 6)) 
-		return -1;
+    uint8_t args[] = {0x0};
+    if (! runCommand(VC0706_DOWNSIZE_STATUS, args, 1, 6)) 
+        return -1;
 
-	return camerabuff[5];
+    return camerabuff[5];
 }
 
 boolean VC0706::setDownsize(uint8_t newsize) 
 {
-	uint8_t args[] = {0x01, newsize};
-	return runCommand(VC0706_DOWNSIZE_CTRL, args, 2, 5);
+    uint8_t args[] = {0x01, newsize};
+    return runCommand(VC0706_DOWNSIZE_CTRL, args, 2, 5);
 }
 
 char * VC0706::getVersion(void) 
 {
-	uint8_t args[] = {0x01};
-	sendCommand(VC0706_GET_VERSION, args, 1);
+    uint8_t args[] = {0x01};
+    sendCommand(VC0706_GET_VERSION, args, 1);
 
-	if (!readResponse(CAMERABUFFSIZ, 200)) 
-		return 0;
-	camerabuff[bufferLen] = 0;
-	return (char *)camerabuff;
+    if (!readResponse(CAMERABUFFSIZ, 200)) 
+        return 0;
+    camerabuff[bufferLen] = 0;
+    return (char *)camerabuff;
 }
 
 void VC0706::OSD(uint8_t x, uint8_t y, char *str) 
 {
-	if (strlen(str) > 14) { str[13] = 0; }
+    if (strlen(str) > 14) { str[13] = 0; }
 
-	uint8_t args[17] = {strlen(str), strlen(str)-1, (y & 0xF) | ((x & 0x3) << 4)};
+    uint8_t args[17] = {strlen(str), strlen(str)-1, (y & 0xF) | ((x & 0x3) << 4)};
 
-	for (uint8_t i=0; i<strlen(str); i++) {
-		char c = str[i];
-		if ((c >= '0') && (c <= '9')) {
-			str[i] -= '0';
-		} else if ((c >= 'A') && (c <= 'Z')) {
-			str[i] -= 'A';
-			str[i] += 10;
-		} else if ((c >= 'a') && (c <= 'z')) {
-			str[i] -= 'a';
-			str[i] += 36;
-		}
-		args[3+i] = str[i];
-	}
-	runCommand(VC0706_OSD_ADD_CHAR, args, strlen(str)+3, 5);
-	printBuff();
+    for (uint8_t i=0; i<strlen(str); i++) {
+        char c = str[i];
+        if ((c >= '0') && (c <= '9')) {
+            str[i] -= '0';
+        } else if ((c >= 'A') && (c <= 'Z')) {
+            str[i] -= 'A';
+            str[i] += 10;
+        } else if ((c >= 'a') && (c <= 'z')) {
+            str[i] -= 'a';
+            str[i] += 36;
+        }
+        args[3+i] = str[i];
+    }
+    runCommand(VC0706_OSD_ADD_CHAR, args, strlen(str)+3, 5);
+    printBuff();
 }
 
 boolean VC0706::setCompression(uint8_t c) 
 {
-	uint8_t args[] = {0x5, 0x1, 0x1, 0x12, 0x04, c};
-	return runCommand(VC0706_WRITE_DATA, args, sizeof(args), 5);
+    uint8_t args[] = {0x5, 0x1, 0x1, 0x12, 0x04, c};
+    return runCommand(VC0706_WRITE_DATA, args, sizeof(args), 5);
 }
 
 uint8_t VC0706::getCompression(void) 
 {
-	uint8_t args[] = {0x4, 0x1, 0x1, 0x12, 0x04};
-	runCommand(VC0706_READ_DATA, args, sizeof(args), 6);
-	printBuff();
-	return camerabuff[5];
+    uint8_t args[] = {0x4, 0x1, 0x1, 0x12, 0x04};
+    runCommand(VC0706_READ_DATA, args, sizeof(args), 6);
+    printBuff();
+    return camerabuff[5];
 }
 
 boolean VC0706::setPTZ(uint16_t wz, uint16_t hz, uint16_t pan, uint16_t tilt) 
 {
-	uint8_t args[] = {0x08, wz >> 8, wz, hz >> 8, wz, pan>>8, pan, tilt>>8, tilt};
-	return (! runCommand(VC0706_SET_ZOOM, args, sizeof(args), 5));
+    uint8_t args[] = {0x08, wz >> 8, wz, hz >> 8, wz, pan>>8, pan, tilt>>8, tilt};
+    return (! runCommand(VC0706_SET_ZOOM, args, sizeof(args), 5));
 }
 
 boolean VC0706::getPTZ(uint16_t &w, uint16_t &h, uint16_t &wz, uint16_t &hz, uint16_t &pan, uint16_t &tilt) 
 {
-	uint8_t args[] = {0x0};
+    uint8_t args[] = {0x0};
   
-	if (! runCommand(VC0706_GET_ZOOM, args, sizeof(args), 16))
-		return false;
-	printBuff();
+    if (! runCommand(VC0706_GET_ZOOM, args, sizeof(args), 16))
+        return false;
+    printBuff();
 
-	w = camerabuff[5];
-	w <<= 8;
-	w |= camerabuff[6];
+    w = camerabuff[5];
+    w <<= 8;
+    w |= camerabuff[6];
 
-	h = camerabuff[7];
-	h <<= 8;
-	h |= camerabuff[8];
+    h = camerabuff[7];
+    h <<= 8;
+    h |= camerabuff[8];
 
-	wz = camerabuff[9];
-	wz <<= 8;
-	wz |= camerabuff[10];
+    wz = camerabuff[9];
+    wz <<= 8;
+    wz |= camerabuff[10];
 
-	hz = camerabuff[11];
-	hz <<= 8;
-	hz |= camerabuff[12];
+    hz = camerabuff[11];
+    hz <<= 8;
+    hz |= camerabuff[12];
 
-	pan = camerabuff[13];
-	pan <<= 8;
-	pan |= camerabuff[14];
+    pan = camerabuff[13];
+    pan <<= 8;
+    pan |= camerabuff[14];
 
-	tilt = camerabuff[15];
-	tilt <<= 8;
-	tilt |= camerabuff[16];
+    tilt = camerabuff[15];
+    tilt <<= 8;
+    tilt |= camerabuff[16];
 
-	return true;
+    return true;
 }
 
 
 boolean VC0706::takePicture() 
 {
-	frameptr = 0;
-	return cameraFrameBuffCtrl(VC0706_STOPCURRENTFRAME); 
+    frameptr = 0;
+    return cameraFrameBuffCtrl(VC0706_STOPCURRENTFRAME); 
 }
 
 boolean VC0706::resumeVideo() 
 {
-	return cameraFrameBuffCtrl(VC0706_RESUMEFRAME); 
+    return cameraFrameBuffCtrl(VC0706_RESUMEFRAME); 
 }
 
 boolean VC0706::TVon() 
 {
-	uint8_t args[] = {0x1, 0x1};
-	return runCommand(VC0706_TVOUT_CTRL, args, sizeof(args), 5);
+    uint8_t args[] = {0x1, 0x1};
+    return runCommand(VC0706_TVOUT_CTRL, args, sizeof(args), 5);
 }
 boolean VC0706::TVoff() 
 {
-	uint8_t args[] = {0x1, 0x0};
-	return runCommand(VC0706_TVOUT_CTRL, args, sizeof(args), 5);
+    uint8_t args[] = {0x1, 0x0};
+    return runCommand(VC0706_TVOUT_CTRL, args, sizeof(args), 5);
 }
 
 boolean VC0706::cameraFrameBuffCtrl(uint8_t command) 
 {
-	uint8_t args[] = {0x1, command};
-	return runCommand(VC0706_FBUF_CTRL, args, sizeof(args), 5);
+    uint8_t args[] = {0x1, command};
+    return runCommand(VC0706_FBUF_CTRL, args, sizeof(args), 5);
 }
 
 uint32_t VC0706::frameLength(void) 
 {
-	uint8_t args[] = {0x01, 0x00};
-	if (!runCommand(VC0706_GET_FBUF_LEN, args, sizeof(args), 9))
-		return 0;
+    uint8_t args[] = {0x01, 0x00};
+    if (!runCommand(VC0706_GET_FBUF_LEN, args, sizeof(args), 9))
+        return 0;
 
-	uint32_t len;
-	len = camerabuff[5];
-	len <<= 8;
-	len |= camerabuff[6];
-	len <<= 8;
-	len |= camerabuff[7];
-	len <<= 8;
-	len |= camerabuff[8];
+    uint32_t len;
+    len = camerabuff[5];
+    len <<= 8;
+    len |= camerabuff[6];
+    len <<= 8;
+    len |= camerabuff[7];
+    len <<= 8;
+    len |= camerabuff[8];
 
-	return len;
+    return len;
 }
-
 
 uint8_t VC0706::available(void) 
 {
-	return bufferLen;
+    return bufferLen;
 }
-
 
 uint8_t * VC0706::readPicture(uint8_t n) 
 {
-	uint8_t args[] = {0x0C, 0x0, 0x0A, 0, 0, frameptr >> 8, frameptr & 0xFF, 
+    uint8_t args[] = {0x0C, 0x0, 0x0A, 0, 0, frameptr >> 8, frameptr & 0xFF, 
                     0, 0, 0, n, CAMERADELAY >> 8, CAMERADELAY & 0xFF};
 
-	if (! runCommand(VC0706_READ_FBUF, args, sizeof(args), 5, false))
-		return 0;
+    if (! runCommand(VC0706_READ_FBUF, args, sizeof(args), 5, false))
+        return 0;
 
-	// read into the buffer PACKETLEN!
-	if (readResponse(n+5, CAMERADELAY) == 0) 
-		return 0;
+    // read into the buffer PACKETLEN!
+    if (readResponse(n+5, CAMERADELAY) == 0) 
+        return 0;
 
-	frameptr += n;
+    frameptr += n;
 
-	return camerabuff;
+    return camerabuff;
 }
 
 boolean VC0706::runCommand(uint8_t cmd, uint8_t *args, uint8_t argn, uint8_t resplen, boolean flushflag) 
 {
-	if (flushflag) {
-		readResponse(100, 10); 
-	}
+    if (flushflag) {
+        readResponse(100, 10); 
+    }
 
-	sendCommand(cmd, args, argn);
-	if (readResponse(resplen, 200) != resplen) 
-		return false;
-	if (! verifyResponse(cmd))
-		return false;
-	return true;
+    sendCommand(cmd, args, argn);
+    if (readResponse(resplen, 200) != resplen) 
+        return false;
+    if (! verifyResponse(cmd))
+        return false;
+    return true;
 }
 
 void VC0706::sendCommand(uint8_t cmd, uint8_t args[] = 0, uint8_t argn = 0) 
 {
-	camera->write((byte)0x56);
-	camera->write((byte)cameraSerial);
-	camera->write((byte)cmd);
+    camera->write((byte)0x56);
+    camera->write((byte)cameraSerial);
+    camera->write((byte)cmd);
 
-	for (uint8_t i=0; i<argn; i++) {
-		camera->write((byte)args[i]);
-	}
+    for (uint8_t i=0; i<argn; i++) {
+        camera->write((byte)args[i]);
+    }
 }
-
 
 uint8_t VC0706::readResponse(uint8_t numbytes, uint8_t timeout) 
 {
-	uint8_t counter = 0;
-	bufferLen=0;
+    uint8_t counter = 0;
+    bufferLen=0;
  
-	while ((timeout != counter) && (bufferLen != numbytes)){
-		if (! camera->available()) {
-			delay(1);
-			counter++;
-			continue;
-		}
-		counter = 0;
-		// there's a byte!
-		camerabuff[bufferLen++] = camera->read();
-	}
-	return bufferLen;
+    while ((timeout != counter) && (bufferLen != numbytes)){
+        if (! camera->available()) {
+            delay(1);
+            counter++;
+            continue;
+        }
+        counter = 0;
+        // there's a byte!
+        camerabuff[bufferLen++] = camera->read();
+    }
+    return bufferLen;
 }
 
 boolean VC0706::verifyResponse(uint8_t command) 
 {
-	if ((camerabuff[0] != 0x76) ||
-		(camerabuff[1] != cameraSerial) ||
-		(camerabuff[2] != command) ||
-		(camerabuff[3] != 0x0))
-			return false;
-	return true;
+    if ((camerabuff[0] != 0x76) ||
+        (camerabuff[1] != cameraSerial) ||
+        (camerabuff[2] != command) ||
+        (camerabuff[3] != 0x0))
+            return false;
+    return true;
 }
 
 void VC0706::printBuff() 
 {
-	for (uint8_t i = 0; i< bufferLen; i++) {
-		Serial.print(" 0x");
-		Serial.print(camerabuff[i], HEX); 
-	}
-	Serial.println();
+    for (uint8_t i = 0; i< bufferLen; i++) {
+        Serial.print(" 0x");
+        Serial.print(camerabuff[i], HEX); 
+    }
+    Serial.println();
 }
--- VC0706.h
+++ VC0706.h
@@ -1,115 +1,173 @@
+/*
+* VC0706.h
+* A library for camera shield(VC0706 chipset)
+*
+* Copyright (c) 2014 seeed technology inc.
+* Copyright (c) 2012, Adafruit Industries.
+*
+* All rights reserved.
+*
+* This library is based on Adafruit's VC0706 Serial Camera Library. Thanks to 
+* their contribution to the code, we modify it to support our Seeed's Camera 
+* Shield module.
+*
+* Below is the introduction of Adafruit's VC0706 Serial Camera module, we add it to here
+* to express our thanks to them.
+*
+* ****************************************************************************
+* This is a library for the Adafruit TTL JPEG Camera (VC0706 chipset)
+*
+* Pick one up today in the adafruit shop!
+* ------> http://www.adafruit.com/products/397
+*
+* These displays use Serial to communicate, 2 pins are required to interface
+*
+* Adafruit invests time and resources providing this open source code, 
+* please support Adafruit and open-source hardware by purchasing 
+* products from Adafruit!
+*
+* Written by Limor Fried/Ladyada for Adafruit Industries.  
+* BSD license, all text above must be included in any redistribution
+* ******************************************************************************
+*
+* Software License Agreement (BSD License)
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holders nor the
+* names of its contributors may be used to endorse or promote products
+* derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
 #ifndef _VC0706_H_
 #define _VC0706_H_
 
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 
-#define VC0706_PTL_BYTE				0x00
-#define VC0706_SERIAL_BYTE			0x01
-#define VC0706_CMD_BYTE				0x02
-#define VC0706_STATUS_BYTE			0x03
+#define VC0706_PTL_BYTE             0x00
+#define VC0706_SERIAL_BYTE          0x01
+#define VC0706_CMD_BYTE             0x02
+#define VC0706_STATUS_BYTE          0x03
 
-#define VC0706_GET_VERSION 			0x11
-#define VC0706_SET_SERIAL_NUMBER	0x21
-#define VC0706_SET_PORT				0x24
-#define VC0706_SYSTEM_RESET  		0x26
-#define VC0706_READ_DATA			0x30
-#define VC0706_WRITE_DATA			0x31
-#define VC0706_READ_FBUF 			0x32
-#define VC0706_WRITE_FBUF			0x33
-#define VC0706_GET_FBUF_LEN 		0x34
-#define VC0706_FBUF_CTRL 			0x36
-#define VC0706_COMM_MOTION_CTRL 	0x37
-#define VC0706_COMM_MOTION_STATUS 	0x38
+#define VC0706_GET_VERSION          0x11
+#define VC0706_SET_SERIAL_NUMBER    0x21
+#define VC0706_SET_PORT             0x24
+#define VC0706_SYSTEM_RESET         0x26
+#define VC0706_READ_DATA            0x30
+#define VC0706_WRITE_DATA           0x31
+#define VC0706_READ_FBUF            0x32
+#define VC0706_WRITE_FBUF           0x33
+#define VC0706_GET_FBUF_LEN         0x34
+#define VC0706_FBUF_CTRL            0x36
+#define VC0706_COMM_MOTION_CTRL     0x37
+#define VC0706_COMM_MOTION_STATUS   0x38
 #define VC0706_COMM_MOTION_DETECTED 0x39
-#define VC0706_MIRROR_CTRL			0x3A
-#define VC0706_MIRROR_STATUS		0x3B
-#define VC0706_COLOR_CTRL			0x3C
-#define VC0706_COLOR_STATUS			0x3D
-#define VC0706_POWER_SAVE_CTRL		0x3E
-#define VC0706_POWER_SAVE_STATUS	0x3F
-#define VC0706_AE_CTRL				0x40
-#define VC0706_AE_STATUS			0x41
-#define VC0706_MOTION_CTRL 			0x42
-#define VC0706_MOTION_STATUS 		0x43
-#define VC0706_TVOUT_CTRL 			0x44
-#define VC0706_OSD_ADD_CHAR 		0x45
-#define VC0706_DOWNSIZE_CTRL 		0x54
-#define VC0706_DOWNSIZE_STATUS 		0x55
-#define VC0706_GET_FLASH_SIZE		0x60
-#define VC0706_ERASE_FLASH_SECTOR	0x61
-#define VC0706_ERASE_FLASH_ALL		0x62
-#define VC0706_READ_LOGO			0x70
-#define VC0706_SET_BIZTMAP			0x71
-#define VC0706_BATCH_WRITE			0x80
+#define VC0706_MIRROR_CTRL          0x3A
+#define VC0706_MIRROR_STATUS        0x3B
+#define VC0706_COLOR_CTRL           0x3C
+#define VC0706_COLOR_STATUS         0x3D
+#define VC0706_POWER_SAVE_CTRL      0x3E
+#define VC0706_POWER_SAVE_STATUS    0x3F
+#define VC0706_AE_CTRL              0x40
+#define VC0706_AE_STATUS            0x41
+#define VC0706_MOTION_CTRL          0x42
+#define VC0706_MOTION_STATUS        0x43
+#define VC0706_TVOUT_CTRL           0x44
+#define VC0706_OSD_ADD_CHAR         0x45
+#define VC0706_DOWNSIZE_CTRL        0x54
+#define VC0706_DOWNSIZE_STATUS      0x55
+#define VC0706_GET_FLASH_SIZE       0x60
+#define VC0706_ERASE_FLASH_SECTOR   0x61
+#define VC0706_ERASE_FLASH_ALL      0x62
+#define VC0706_READ_LOGO            0x70
+#define VC0706_SET_BIZTMAP          0x71
+#define VC0706_BATCH_WRITE          0x80
 
-#define VC0706_STOPCURRENTFRAME 	0x0
-#define VC0706_STOPNEXTFRAME 		0x1
-#define VC0706_RESUMEFRAME 			0x3
-#define VC0706_STEPFRAME 			0x2
+#define VC0706_STOPCURRENTFRAME     0x0
+#define VC0706_STOPNEXTFRAME        0x1
+#define VC0706_RESUMEFRAME          0x3
+#define VC0706_STEPFRAME            0x2
 
-#define VC0706_640x480 				0x00
-#define VC0706_320x240 				0x11
-#define VC0706_160x120 				0x22
+#define VC0706_640x480              0x00
+#define VC0706_320x240              0x11
+#define VC0706_160x120              0x22
 
-#define VC0706_MOTIONCONTROL 		0x0
-#define VC0706_UARTMOTION 			0x01
-#define VC0706_ACTIVATEMOTION 		0x01
+#define VC0706_MOTIONCONTROL        0x0
+#define VC0706_UARTMOTION           0x01
+#define VC0706_ACTIVATEMOTION       0x01
 
-#define VC0706_SET_ZOOM 			0x52
-#define VC0706_GET_ZOOM 			0x53
+#define VC0706_SET_ZOOM             0x52
+#define VC0706_GET_ZOOM             0x53
 
-#define CAMERABUFFSIZ 				100
-#define CAMERADELAY 				10
+#define CAMERABUFFSIZ               100
+#define CAMERADELAY                 10
 
-#define VC0706_SERIAL_NUMBER		0x00
+#define VC0706_SERIAL_NUMBER        0x00
 
 class VC0706 {
 public:
-	VC0706(SoftwareSerial *ser);
-	boolean begin(uint16_t baud = 38400);
-	boolean reset(void);
-	boolean TVon(void);
-	boolean TVoff(void);
-	boolean takePicture(void);
-	uint8_t *readPicture(uint8_t n);
-	boolean resumeVideo(void);
-	uint32_t frameLength(void);
-	char *getVersion(void);
-	uint8_t available();
-	uint8_t getDownsize(void);
-	boolean setDownsize(uint8_t);
-	uint8_t getImageSize();
-	boolean setImageSize(uint8_t);
-	boolean getMotionDetect();
-	uint8_t getMotionStatus(uint8_t);
-	boolean motionDetected();
-	boolean setMotionDetect(boolean f);
-	boolean setMotionStatus(uint8_t x, uint8_t d1, uint8_t d2);
-	boolean cameraFrameBuffCtrl(uint8_t command);
-	uint8_t getCompression();
-	boolean setCompression(uint8_t c);
-	  
-	boolean getPTZ(uint16_t &w, uint16_t &h, uint16_t &wz, uint16_t &hz, uint16_t &pan, uint16_t &tilt);
-	boolean setPTZ(uint16_t wz, uint16_t hz, uint16_t pan, uint16_t tilt);
+    VC0706(SoftwareSerial *ser);
+    boolean begin(uint16_t baud = 38400);
+    boolean reset(void);
+    boolean TVon(void);
+    boolean TVoff(void);
+    boolean takePicture(void);
+    uint8_t *readPicture(uint8_t n);
+    boolean resumeVideo(void);
+    uint32_t frameLength(void);
+    char *getVersion(void);
+    uint8_t available();
+    uint8_t getDownsize(void);
+    boolean setDownsize(uint8_t);
+    uint8_t getImageSize();
+    boolean setImageSize(uint8_t);
+    boolean getMotionDetect();
+    uint8_t getMotionStatus(uint8_t);
+    boolean motionDetected();
+    boolean setMotionDetect(boolean f);
+    boolean setMotionStatus(uint8_t x, uint8_t d1, uint8_t d2);
+    boolean cameraFrameBuffCtrl(uint8_t command);
+    uint8_t getCompression();
+    boolean setCompression(uint8_t c);
+      
+    boolean getPTZ(uint16_t &w, uint16_t &h, uint16_t &wz, uint16_t &hz, uint16_t &pan, uint16_t &tilt);
+    boolean setPTZ(uint16_t wz, uint16_t hz, uint16_t pan, uint16_t tilt);
 
-	void OSD(uint8_t x, uint8_t y, char *s);
+    void OSD(uint8_t x, uint8_t y, char *s);
   
 private:
-	uint8_t _rx, _tx;
-	uint16_t baud;
-	uint8_t cameraSerial;
-	uint8_t camerabuff[CAMERABUFFSIZ+1];
-	uint8_t bufferLen;
-	uint16_t frameptr;
-	
-	SoftwareSerial *camera;
+    uint8_t _rx, _tx;
+    uint16_t baud;
+    uint8_t cameraSerial;
+    uint8_t camerabuff[CAMERABUFFSIZ+1];
+    uint8_t bufferLen;
+    uint16_t frameptr;
+    
+    SoftwareSerial *camera;
 
-	boolean runCommand(uint8_t cmd, uint8_t args[], uint8_t argn, uint8_t resp, boolean flushflag = true); 
-	void sendCommand(uint8_t cmd, uint8_t args[], uint8_t argn); 
-	uint8_t readResponse(uint8_t numbytes, uint8_t timeout);
-	boolean verifyResponse(uint8_t command);
-	void printBuff(void);
+    boolean runCommand(uint8_t cmd, uint8_t args[], uint8_t argn, uint8_t resp, boolean flushflag = true); 
+    void sendCommand(uint8_t cmd, uint8_t args[], uint8_t argn); 
+    uint8_t readResponse(uint8_t numbytes, uint8_t timeout);
+    boolean verifyResponse(uint8_t command);
+    void printBuff(void);
 };
 
 #endif
